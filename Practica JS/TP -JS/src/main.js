1462007a45bac2d6e2362e93d0af4eda5dc80e3f
Eduar <83920925+eduarchilon@users.noreply.github.com>
2022-05-23 12:01:55 -0300

    feat/modulos

--- Practica JS/TP -JS/src/main.js
+++ Practica JS/TP -JS/src/main.js
@@ -1,9 +1,11 @@
+import { adicion, restar } from "./app/ejercicio01.js";
+import { hayParOPareja } from "./app/ejercicio02.js";
+import { esPalindromo } from "./app/ejercicio03.js"
+import { convertirARomano } from "./app/ejercicio04.js"; 
+
 /** Add event listener **/
 const form = document.getElementById('form');
 form.addEventListener('submit', formFunctions)
-
-/** Seleccion el resultado**/
-let solution = document.getElementById('solution__container')
 
 /** Creo un evento on change al select de sumar y restar **/
 const select = document.getElementById('select')
@@ -20,13 +22,13 @@ select.addEventListener('change', function(e){
     }
 })
 
-//Funciones del evento submit del formulario
+/*Funciones del evento submit del formulario*/
 function formFunctions(e){
     e.preventDefault();
-    let select = document.getElementById('select').value;
+    let selectValue = document.getElementById('select').value
     let input = document.getElementById('text').value
     if(input!=='')
-    switch (select){
+    switch (selectValue){
         case 'value2':
             hayParOPareja(input);
             break;
@@ -39,10 +41,9 @@ function formFunctions(e){
         default:
             break;
     }
-    // console.log(select.options[select'.selectedIndex].text)
 }
 
-//creacion del input y los botones de sumar y restar
+/*creacion del input y los botones de sumar y restar*/
 function createInputNumberAndButton(){
     let otherInput = document.getElementById("other-input");
     otherInput.innerHTML = `
@@ -58,211 +59,4 @@ function createInputNumberAndButton(){
     const rest = document.getElementById('btn-rest');
     sum.addEventListener('click', adicion);
     rest.addEventListener('click', restar);
-}
-
-/*
-EJERCIO 01
-*/
-/*FUNCIONES CON INPUTS */
-function adicion(input, number){
-    let arrayString = [];
-    let arrayNumber = [];
-    input = document.getElementById('text').value
-    number = parseInt(document.getElementById('number').value)
-    arrayString = input.split(',')
-    arrayString.forEach(element => {
-        arrayNumber.push(parseInt(element))
-    });
-    let newNumber = parseInt(arrayNumber.join(''))
-    let resultado;
-    let resultadoArray = [];
-    let resultadoArrayNumber = [];
-    if(!isNaN(newNumber) && newNumber>number){
-        resultado = newNumber + number;
-        resultadoArray = resultado.toString().split('')
-        for (let element of resultadoArray) {
-            resultadoArrayNumber.push(parseInt(element));
-            solution.innerHTML = `
-                <h5>Array:</h5>
-                </br>
-                <p>[${arrayNumber}]</p>
-                </br>
-                </br>
-                <h5>Numero:</h5>
-                <p>${number}</p>
-                </br>
-                </br>
-                <h5>Resultado:</h5>
-                </br>
-                <p>[${resultadoArrayNumber}]</p>
-                ` ;
-    }
-}else{
-            solution.innerHTML = `
-                    <p>¡Debe ingresar solo numeros!</p>
-                ` 
-        console.log("Error")
-        
-        
-    }
-    }
-
-function restar(array, number){
-    let arrayString = [];
-    let arrayNumber = [];
-    input = document.getElementById('text').value
-    number = parseInt(document.getElementById('number').value)
-    arrayString = input.split(',')
-    arrayString.forEach(element => {
-        arrayNumber.push(parseInt(element))
-    });
-    let newNumber = parseInt(arrayNumber.join(''))
-    let resultado;
-    let resultadoArray = [];
-    let resultadoArrayNumber = [];
-    if(!isNaN(newNumber) && newNumber>number){
-        resultado = newNumber - number;
-        resultadoArray = resultado.toString().split('')
-        for (let element of resultadoArray) {
-            resultadoArrayNumber.push(parseInt(element));
-            solution.innerHTML = `
-                <h5>Array:</h5>
-                </br>
-                <p>[${arrayNumber}]</p>
-                </br>
-                </br>
-                <h5>Numero:</h5>
-                <p>${number}</p>
-                </br>
-                </br>
-                <h5>Resultado:</h5>
-                </br>
-                <p>[${resultadoArrayNumber}]</p>
-                ` ;
-    }
-}else{
-            solution.innerHTML = `
-                    <p>¡Debe ingresar solo numeros!</p>
-                ` 
-        console.log("Error")
-        
-        
-    }
-}
-
-/*
-EJERCICIO 02 
-*/
-
-function hayParOPareja(array){
-    if (!isNaN(array)) {
-        let cadena = array.split('')
-        let nuevoArray = [];
-        cadena.forEach(element => {
-            nuevoArray.push(parseInt(element))
-        });
-        let resultado;
-        let pares = [];
-        nuevoArray.sort();
-        // console.log(nuevoArray)
-        for (let i = 0; i < nuevoArray.length; i++) {
-            if (nuevoArray[i] % 2 === 0 || nuevoArray[i + 1] === nuevoArray[i]) {
-                pares.push(nuevoArray[i])
-            }else{
-                solution.innerHTML = `
-                <p>¡No existen numeros pares ni numeros con pareja!</p>
-                `
-            }
-        }
-        console.log(pares)
-        let par = [];
-        let parejaSumada = [];
-        for (let i = 0; i < pares.length; i++) {
-            if(pares[i]%2 === 0){
-                par.push(pares[i])
-                console.log(par)
-            }if (pares[i]%2 !==0){
-                parejaSumada.push(pares[i])
-                console.log(parejaSumada)
-            }
-            solution.innerHTML = `
-                <p>Existen numeros pares o que son pareja</p>
-                </br>
-                </br>
-                <h5>Numeros Pares:</h5>
-                </br>
-                <p>${par}</p>
-                </br>
-                <h5>Numeros con pareja:</h5>
-                </br>
-                <p>${parejaSumada}</p>
-            `
-        }
-    }
-}
-
-/*
-EJERCICIO 03 
-*/
-function esPalindromo(string){
-    let array;
-    //s = espacios en blanco y tabulaciones, g = global y "" que lo reemplaza por espacios en blanco con cadena vacia
-    array = string.toLowerCase().replace(/\s+/g, '').split('')
-    console.log(array)
-    let reves = [];
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === array.reverse()[i]) {
-            reves.push(array[i])
-        }
-    }
-    if (reves.length === array.length) {
-        solution.innerHTML = `
-        <p>¡Es palindromo!</p>
-    `
-    }
-    else{
-        solution.innerHTML = `
-        <p>¡No es palindromo!</p>
-    `
-    }
-}
-
-/*
-EJERCICIO 04 
-*/
-function convertirARomano(num) { 
-    let converter = { 
-        M: 1000, 
-        CM: 900, 
-        D: 500, 
-        CD: 400, 
-        C: 100, 
-        XC: 90, 
-        L: 50, 
-        XL: 40, 
-        X: 10, 
-        IX: 9, 
-        V: 5, 
-        IV: 4, 
-        I: 1 
-    } 
-    let romano;
-    let prop;
-    romano = '';  
-    if(num<4000){
-        for (prop in converter) { 
-            while (num >= converter[prop]) { 
-                romano += prop; 
-                num -= converter[prop]; 
-            } 
-        }
-        console.log(romano)
-        solution.innerHTML = `
-            <p> ${romano}</p>
-        `
-    }else{
-        solution.innerHTML = `
-            <p>¡No existe ese numero o no se puede exceder el maximo de 3999!</p>
-        `
-    }
 }
